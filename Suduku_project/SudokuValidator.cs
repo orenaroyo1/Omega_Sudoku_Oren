5d910fe82a3031d16876c76e7ec8b861b30d6239
orenaroyo1 <74186543+orenaroyo1@users.noreply.github.com>
2026-02-04 00:03:55 +0200

    to fix all git

--- Suduku_project/SudokuValidator.cs
+++ Suduku_project/SudokuValidator.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace Suduku_project
 {
-    internal class SudokuValidator
+    internal static class SudokuValidator
     {
+        private const int BoardSize = 81;//
+        //
+        public static bool IsValidFormat(string SudokuString)
+        {
+            if (string.IsNullOrEmpty(SudokuString) || SudokuString.Length != BoardSize)
+            {
+                return false;
+            }
+
+            foreach (char c in SudokuString)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        //
+        public static bool IsBoardLegal(string SudokuString)
+        {
+            int[] rows = new int[9];
+            int[] cols = new int[9];
+            int[] boxes = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int charIndex = i * 9 + j;
+                    int val = SudokuString[charIndex] - '0';
+
+                    if (val > 0)
+                    {
+                        int boxIndex = (i / 3) * 3 + (j / 3);
+                        int mask = 1 << (val - 1); 
+
+                        if ((rows[i] & mask) != 0 || (cols[j] & mask) != 0 || (boxes[boxIndex] & mask) != 0)
+                        {
+                            return false; 
+                        }
+
+                        rows[i] |= mask;
+                        cols[j] |= mask;
+                        boxes[boxIndex] |= mask;
+                    }
+                }
+            }
+
+            return true;
+        }
     }
 }
